0df9811669bbf5d1de1d51ada84234cd93e4837a
tr1um7h <tr1um7h@gmail.com>
2025-12-07 16:11:30 +0800

    [29] feature: support gossip pubsub

--- 29_network/examples/p2p_chat.rs
+++ 29_network/examples/p2p_chat.rs
@@ -2,13 +2,20 @@ use anyhow::Result;
 use futures::StreamExt;
 use libp2p::{
     core::upgrade,
-    floodsub::{self, Floodsub, FloodsubEvent, Topic},
+    // floodsub::{self, Floodsub, FloodsubEvent, Topic},
+    gossipsub::{
+        self, Gossipsub, GossipsubConfig, GossipsubEvent, IdentTopic, MessageAuthenticity,
+    },
     identity,
     mdns::{Mdns, MdnsEvent},
     noise,
     swarm::{NetworkBehaviourEventProcess, SwarmBuilder, SwarmEvent},
     tcp::TokioTcpConfig,
-    yamux, NetworkBehaviour, PeerId, Swarm, Transport,
+    yamux,
+    NetworkBehaviour,
+    PeerId,
+    Swarm,
+    Transport,
 };
 use std::borrow::Cow;
 use tokio::io::{stdin, AsyncBufReadExt, BufReader};
@@ -19,7 +26,8 @@ use tokio::io::{stdin, AsyncBufReadExt, BufReader};
 #[behaviour(event_process = true)]
 struct ChatBehavior {
     /// flood subscription，比较浪费带宽，gossipsub 是更好的选择
-    floodsub: Floodsub,
+    // floodsub: Floodsub,
+    gossipsub: Gossipsub,
     /// 本地节点发现机制
     mdns: Mdns,
     // 在 behavior 结构中，你也可以放其它数据，但需要 ignore
@@ -29,20 +37,27 @@ struct ChatBehavior {
 
 impl ChatBehavior {
     /// 创建一个新的 ChatBehavior
-    pub async fn new(id: PeerId) -> Result<Self> {
+    pub async fn new(privacy: MessageAuthenticity, config: GossipsubConfig) -> Result<Self> {
         Ok(Self {
             mdns: Mdns::new(Default::default()).await?,
-            floodsub: Floodsub::new(id),
+            // floodsub: Floodsub::new(id),
+            gossipsub: Gossipsub::new(privacy, config).unwrap(),
         })
     }
 }
 
-impl NetworkBehaviourEventProcess<FloodsubEvent> for ChatBehavior {
+impl NetworkBehaviourEventProcess<GossipsubEvent> for ChatBehavior {
     // 处理 floodsub 产生的消息
-    fn inject_event(&mut self, event: FloodsubEvent) {
-        if let FloodsubEvent::Message(msg) = event {
-            let text = String::from_utf8_lossy(&msg.data);
-            println!("{:?}: {:?}", msg.source, text);
+    fn inject_event(&mut self, event: GossipsubEvent) {
+        // if let FloodsubEvent::Message(msg) = event {
+        if let GossipsubEvent::Message {
+            propagation_source,
+            message_id: _,
+            message,
+        } = event
+        {
+            let text = String::from_utf8_lossy(&message.data);
+            println!("{:?}: {:?}", propagation_source, text);
         }
     }
 }
@@ -54,14 +69,16 @@ impl NetworkBehaviourEventProcess<MdnsEvent> for ChatBehavior {
                 // 把 mdns 发现的新的 peer 加入到 floodsub 的 view 中
                 for (id, addr) in list {
                     println!("Got peer: {} with addr {}", &id, &addr);
-                    self.floodsub.add_node_to_partial_view(id);
+                    // self.floodsub.add_node_to_partial_view(id);
+                    self.gossipsub.add_explicit_peer(&id);
                 }
             }
             MdnsEvent::Expired(list) => {
                 // 把 mdns 发现的离开的 peer 加入到 floodsub 的 view 中
                 for (id, addr) in list {
                     println!("Removed peer: {} with addr {}", &id, &addr);
-                    self.floodsub.remove_node_from_partial_view(&id);
+                    // self.floodsub.remove_node_from_partial_view(&id);
+                    self.gossipsub.remove_explicit_peer(&id);
                 }
             }
         }
@@ -77,7 +94,8 @@ async fn main() -> Result<()> {
     };
 
     // 创建 floodsub topic
-    let topic = floodsub::Topic::new(name);
+    // let topic = floodsub::Topic::new(name);
+    let topic = gossipsub::Topic::new(name);
 
     // 创建 swarm
     let mut swarm = create_swarm(topic.clone()).await?;
@@ -92,7 +110,8 @@ async fn main() -> Result<()> {
         tokio::select! {
             line = stdin.next_line() => {
                 let line = line?.expect("stdin closed");
-                swarm.behaviour_mut().floodsub.publish(topic.clone(), line.as_bytes());
+                // swarm.behaviour_mut().floodsub.publish(topic.clone(), line.as_bytes());
+                swarm.behaviour_mut().gossipsub.publish(topic.clone(), line.as_bytes()).unwrap();
             }
             event = swarm.select_next_some() => {
                 if let SwarmEvent::NewListenAddr { address, .. } = event {
@@ -103,7 +122,7 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn create_swarm(topic: Topic) -> Result<Swarm<ChatBehavior>> {
+async fn create_swarm(topic: IdentTopic) -> Result<Swarm<ChatBehavior>> {
     // 创建 identity（密钥对）
     let id_keys = identity::Keypair::generate_ed25519();
     let peer_id = PeerId::from(id_keys.public());
@@ -120,10 +139,13 @@ async fn create_swarm(topic: Topic) -> Result<Swarm<ChatBehavior>> {
         .multiplex(yamux::YamuxConfig::default())
         .boxed();
 
+    let msg_auth = MessageAuthenticity::Signed(id_keys);
+    let cfg = GossipsubConfig::default();
     // 创建 chat behavior
-    let mut behavior = ChatBehavior::new(peer_id).await?;
+    let mut behavior = ChatBehavior::new(msg_auth, cfg).await?;
     // 订阅某个主题
-    behavior.floodsub.subscribe(topic.clone());
+    // behavior.floodsub.subscribe(topic.clone());
+    behavior.gossipsub.subscribe(&topic).unwrap();
     // 创建 swarm
     let swarm = SwarmBuilder::new(transport, behavior, peer_id)
         .executor(Box::new(|fut| {
