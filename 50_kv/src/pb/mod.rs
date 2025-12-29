dac46136fc1238aa983bfd1bdd10c004942d97cd
tr1um7h <tr1um7h@gmail.com>
2025-12-29 11:30:59 +0800

    [50] 22: add skeleton for abi/stream trait

---
+++ 50_kv/src/pb/mod.rs
@@ -0,0 +1,39 @@
+pub mod abi;
+
+use abi::{command_request::RequestData, *};
+
+impl CommandRequest {
+    pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
+        Self {
+            request_data: Some(RequestData::Hset(Hset {
+                table: table.into(),
+                pair: Some(Kvpair::new(key, value)),
+            })),
+        }
+    }
+}
+
+impl Kvpair {
+    pub fn new(key: impl Into<String>, value: Value) -> Self {
+        Self {
+            key: key.into(),
+            value: Some(value),
+        }
+    }
+}
+
+impl From<String> for Value {
+    fn from(s: String) -> Self {
+        Self {
+            value: Some(value::Value::String(s)),
+        }
+    }
+}
+
+impl From<&str> for Value {
+    fn from(s: &str) -> Self {
+        Self {
+            value: Some(value::Value::String(s.into())),
+        }
+    }
+}
